8ccb992a0365a1df18bb6b60f6736c8bf7dcd255
Marlen-Lizeth-Estrada-Lopez <marlen.estrada@correounivalle.edu.co>
2024-06-19 02:08:47 -0500

    Se añadieron los metodos faltantes menos el de guardado en archivo y se realizo el menú en el app

--- Ejercicio_2/src/App.java
+++ Ejercicio_2/src/App.java
@@ -1,5 +1,56 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hola mundo");
+        Gestion_Inventario gestionInventario = new Gestion_Inventario();
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("\nOpciones del sistema de gestión de inventario:");
+            System.out.println("1. Agregar producto");
+            System.out.println("2. Actualizar cantidad");
+            System.out.println("3. Eliminar producto");
+            System.out.println("4. Buscar producto");
+            System.out.println("5. Guardar inventario");
+            System.out.println("6. Salir");
+
+            System.out.print("Seleccione una opción: ");
+            String opcion = scanner.nextLine();
+
+            switch (opcion) {
+                case "1":
+                    System.out.print("Ingrese el código del producto: ");
+                    String codigoAgregar = scanner.nextLine();
+                    System.out.print("Ingrese la cantidad: ");
+                    int cantidadAgregar = Integer.parseInt(scanner.nextLine());
+                    gestionInventario.agregarProducto(codigoAgregar, cantidadAgregar);
+                    break;
+                case "2":
+                    System.out.print("Ingrese el código del producto: ");
+                    String codigoActualizar = scanner.nextLine();
+                    System.out.print("Ingrese la nueva cantidad: ");
+                    int cantidadActualizar = Integer.parseInt(scanner.nextLine());
+                    gestionInventario.actualizarCantidad(codigoActualizar, cantidadActualizar);
+                    break;
+                case "3":
+                    System.out.print("Ingrese el código del producto: ");
+                    String codigoEliminar = scanner.nextLine();
+                    gestionInventario.eliminarProducto(codigoEliminar);
+                    break;
+                case "4":
+                    System.out.print("Ingrese el código del producto: ");
+                    String codigoBuscar = scanner.nextLine();
+                    gestionInventario.buscarProducto(codigoBuscar);
+                    break;
+                case "5":
+                    break;
+                case "6":
+                    System.out.println("Saliendo del sistema de gestión de inventario.");
+                    scanner.close();
+                    return;
+                default:
+                    System.out.println("Opción no válida. Por favor, seleccione una opción válida.");
+            }
+        }
     }
 }
